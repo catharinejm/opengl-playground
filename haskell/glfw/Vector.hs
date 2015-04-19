e238be7e9997aeb5236a7c47c01e47a68aca26ff
Jon Distad <jon.distad@gmail.com>
2015-04-19 18:11:25 -0400

    move glfw demo

--- haskell/glfw/Vector.hs
+++ haskell/glfw/Vector.hs
@@ -4,6 +4,7 @@ module Main where
 
 import Control.Applicative
 import System.FilePath ((</>))
+import qualified Data.Vector.Storable as V
 
 import qualified Graphics.Rendering.OpenGL as GL
 import Graphics.Rendering.OpenGL (($=))
@@ -21,37 +22,35 @@ main = do
 
 initResources :: IO Program
 initResources = do
-  -- compile vertex shader
   vs <- U.loadShader GL.VertexShader $ shaderPath </> "triangle.v.glsl"
   fs <- U.loadShader GL.FragmentShader $ shaderPath </> "triangle.f.glsl"
   p <- U.linkShaderProgram [vs, fs]
   GL.blend $= GL.Enabled
   GL.blendFunc $= (GL.SrcAlpha, GL.OneMinusSrcAlpha)
-  (Program p) <$> GL.get (GL.attribLocation p "coord2d")
-    <*> U.makeBuffer GL.ArrayBuffer vertices
+  Program p <$> GL.get (GL.attribLocation p "coord2d")
 
 draw :: Program -> GLFW.Window -> IO ()
-draw (Program program attrib buf) win = do
+draw (Program program attrib) win = do
   GL.clearColor $= GL.Color4 1 1 1 1
   GL.clear [GL.ColorBuffer]
   (width, height) <- GLFW.getFramebufferSize win
   GL.viewport $= (GL.Position 0 0, GL.Size (fromIntegral width) (fromIntegral height))
-
   GL.currentProgram $= Just program
   GL.vertexAttribArray attrib $= GL.Enabled
-  GL.bindBuffer GL.ArrayBuffer $= Just buf
-  GL.vertexAttribPointer attrib $=
-    (GL.ToFloat, GL.VertexArrayDescriptor 2 GL.Float 0 U.offset0)
-  GL.drawArrays GL.Triangles 0 3 -- 3 vertices
+  V.unsafeWith vertices $ \ptr ->
+    GL.vertexAttribPointer attrib $=
+      (GL.ToFloat, GL.VertexArrayDescriptor 2 GL.Float 0 ptr)
+  GL.drawArrays GL.Triangles 0 3
   GL.vertexAttribArray attrib $= GL.Disabled
 
-data Program = Program GL.Program GL.AttribLocation GL.BufferObject
+
+data Program = Program GL.Program GL.AttribLocation
 
 shaderPath :: FilePath
 shaderPath = "."
 
-vertices :: [Float]
-vertices = [ 0.0, 0.8
-           ,-0.8,-0.8
-           , 0.8,-0.8
-           ]
+vertices :: V.Vector Float
+vertices = V.fromList [ 0.0, 0.8
+                      ,-0.8,-0.8
+                      , 0.8,-0.8
+                      ]
