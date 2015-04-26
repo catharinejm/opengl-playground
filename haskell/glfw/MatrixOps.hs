8c88bffbe0674fdd1cec0795bb7b8d3f7fd4c5f8
Jon Distad <jon.distad@gmail.com>
2015-04-26 19:00:10 -0400

    manual row major switch

--- haskell/glfw/MatrixOps.hs
+++ haskell/glfw/MatrixOps.hs
@@ -3,11 +3,11 @@ module MatrixOps where
 import Numeric.LinearAlgebra.HMatrix
 
 
-colMaj ((r, c), v) = ((c, r), v)
--- colMaj = id
+updateIdent = accum (ident 4) const
 
-updateIdent :: [((Int, Int), Float)] -> Matrix Float
-updateIdent vals = accum (ident 4) const (map colMaj vals)
+
+-- All constructed matrices below use Row Major indexing since GLUtil assumes RowMajor
+-- openglbook.com Chapter 4 has Column Major matrices, so these are flipped
 
 
 rotateAboutX :: Matrix Float -> Float -> Matrix Float
@@ -15,8 +15,8 @@ rotateAboutX m angle =
   let sine = sin angle
       cosine = cos angle
       rot = updateIdent [((1,1), cosine)
-                        ,((1,2), -sine)
-                        ,((2,1), sine)
+                        ,((1,2), sine)
+                        ,((2,1), -sine)
                         ,((2,2), cosine)
                         ]
   in m <> rot
@@ -26,8 +26,8 @@ rotateAboutY m angle =
   let sine = sin angle
       cosine = cos angle
       rot = updateIdent [((0,0), cosine)
-                        ,((2,0), sine)
-                        ,((0,2), -sine)
+                        ,((2,0), -sine)
+                        ,((0,2), sine)
                         ,((2,2), cosine)
                         ]
   in m <> rot
@@ -37,8 +37,8 @@ rotateAboutZ m angle =
   let sine = sin angle
       cosine = cos angle
       rot = updateIdent [((0,0), cosine)
-                        ,((0,1), -sine)
-                        ,((1,0), sine)
+                        ,((0,1), sine)
+                        ,((1,0), -sine)
                         ,((1,1), cosine)
                         ]
   in m <> rot
@@ -47,19 +47,19 @@ scale :: Matrix Float -> Float -> Float -> Float -> Matrix Float
 scale m x y z = m <> (diag $ 4 |> [x,y,z,1])
 
 translate :: Matrix Float -> Float -> Float -> Float -> Matrix Float
-translate m x y z = m <> updateIdent [((3,0), x)
-                                     ,((3,1), y)
-                                     ,((3,2), z)
+translate m x y z = m <> updateIdent [((0,3), x)
+                                     ,((1,3), y)
+                                     ,((2,3), z)
                                      ]
 
 makeProjMatrix :: Float -> Float -> Float -> Float -> Matrix Float
 makeProjMatrix fovy aspect near far =
-  accum zeros const $ map colMaj [((0,0), xscale)
-                                 ,((1,1), yscale)
-                                 ,((2,2), -((far + near) / frustum))
-                                 ,((2,3), -1)
-                                 ,((3,2), -((2 * near * far) / frustum))
-                                 ]
+  accum zeros const [((0,0), xscale)
+                    ,((1,1), yscale)
+                    ,((2,2), -((far + near) / frustum))
+                    ,((2,3), -((2 * near * far) / frustum))
+                    ,((3,2), -1)
+                    ]
   where
     zeros = (4><4) $ repeat 0.0
     yscale = cotan (fovy / 2)
